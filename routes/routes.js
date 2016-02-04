c0e06f7c3d4b5acfbbf94a7780b5a05760eb3cf7
Renevey Gilles ZAS <gilles.renevey@zas.admin.ch>
2016-02-04 18:32:24 +0100

    init

---
+++ routes/routes.js
@@ -0,0 +1,28 @@
+var appRouter = function(app) {  
+  app.get("/", function(req, res) {
+    res.send("Hello World");
+  });
+  app.get("/account", function(req, res) {
+      var accountMock = {
+          "username": "ren",
+          "password": "1234",
+          "twitter": "@ren"
+      }
+      if(!req.query.username) {
+          return res.send({"status": "error", "message": "missing username"});
+      } else if(req.query.username != accountMock.username) {
+          return res.send({"status": "error", "message": "wrong username"});
+      } else {
+          return res.send(accountMock);
+      }
+  });
+  app.post("/account", function(req, res) {
+      if(!req.body.username || !req.body.password || !req.body.twitter) {
+          return res.send({"status": "error", "message": "missing a parameter"});
+      } else {
+          return res.send(req.body);
+      }
+  });
+}
+
+module.exports = appRouter;
