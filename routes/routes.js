e9decac8e236e942f2e0d23705d271c83038acea
Gilles Renevey <Gilles Renevey>
2016-02-09 22:47:06 +0100

    add auth pour google

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,17 @@
 var appRouter = function(app) {  
-  app.get("/", function(req, res) {
-    res.send("Hello World");
+
+  // règle le problème dans chrome d'auth.
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
+
+  app.get("/", function(req, res) {
+    res.send("Server ready");
+  });
+  
+  // exemple de service REST GET
   app.get("/account", function(req, res) {
       var accountMock = {
           "username": "ren",
@@ -16,11 +26,14 @@ var appRouter = function(app) {
           return res.send(accountMock);
       }
   });
-  app.post("/account", function(req, res) {
-      if(!req.body.username || !req.body.password || !req.body.twitter) {
+  
+  // appel pour effectuer un calcul
+  app.post("/calcul", function(req, res) {
+      if(!req.body.data || !req.body.ci) {
           return res.send({"status": "error", "message": "missing a parameter"});
       } else {
-          return res.send(req.body);
+          var result = Number(req.body.ci[0]) + Number(req.body.ci[1]);
+          return res.send("result: "+result);
       }
   });
 }
